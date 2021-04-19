dc42a64b961f6b01339a26a4e8d420c6d3f52281
gupta <nandinee.gupta_cs18@gla.ac.in>
2021-04-20 00:41:52 +0530

    add hero component

--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Hero from "../components/Hero"
 
 const Rooms = () => {
     return (
-        <div>
-            hello from room
-        </div>
+        <Hero hero="roomsHero"></Hero>
     )
 }
 
