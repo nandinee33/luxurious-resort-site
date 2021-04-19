731d25a37f16e551958212e1d23e7e8bc27294c9
gupta <nandinee.gupta_cs18@gla.ac.in>
2021-04-20 00:53:01 +0530

    add banner

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+
 
 export default function Home() {
     return (
-        <Hero></Hero>
+        <Hero>
+            <Banner
+                title="Luxurious Rooms"
+                subtitle="deluxe rooms starting at Rs.1000"
+                >
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
