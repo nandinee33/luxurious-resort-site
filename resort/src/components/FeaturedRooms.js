ea7191e6ccca3958a2e1d4ca052c9fec6d7cf47a
gupta <nandinee.gupta_cs18@gla.ac.in>
2021-04-20 21:28:01 +0530

    make array for featured room

--- resort/src/components/FeaturedRooms.js
+++ resort/src/components/FeaturedRooms.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {RoomContext} from "../context";
 
 export default class FeaturedRooms extends Component {
+
+    static contextType = RoomContext;
+
     render() {
+        const {featuredRooms: rooms} = this.context
+        console.log(rooms);
+
         return (
             <div>
-                hello from featured romms
+                hello featured romms
             </div>
         )
     }
