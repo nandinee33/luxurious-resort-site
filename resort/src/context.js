aebb484917fbb6aba2b1e7670abf9d17aa9a9bcf
gupta <nandinee.gupta_cs18@gla.ac.in>
2021-04-22 22:42:51 +0530

    price filter complete

--- resort/src/context.js
+++ resort/src/context.js
@@ -84,6 +84,7 @@ export default class RoomProvider extends Component {
         // transform values
         // get capacity
         capacity = parseInt(capacity);
+        price = parseInt(price);
 
         // filter by type
         if (type !== "all") {
@@ -94,6 +95,9 @@ export default class RoomProvider extends Component {
         if (capacity !== 1) {
           tempRooms = tempRooms.filter(room => room.capacity >= capacity);
         }
+
+        // filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price);
 
         this.setState({
           sortedRooms: tempRooms
