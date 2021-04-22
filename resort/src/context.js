55be6255b4f561ff7578a10770ce67e358a7efe0
gupta <nandinee.gupta_cs18@gla.ac.in>
2021-04-22 22:54:01 +0530

    complete set filtering

--- resort/src/context.js
+++ resort/src/context.js
@@ -99,6 +99,21 @@ export default class RoomProvider extends Component {
         // filter by price
         tempRooms = tempRooms.filter(room => room.price <= price);
 
+        //filter by size
+        tempRooms = tempRooms.filter(
+          room => room.size >= minSize && room.size <= maxSize
+        );
+
+        //filter by breakfast
+        if (breakfast) {
+          tempRooms = tempRooms.filter(room => room.breakfast === true);
+        }
+        
+        //filter by pets
+        if (pets) {
+          tempRooms = tempRooms.filter(room => room.pets === true);
+        }
+
         this.setState({
           sortedRooms: tempRooms
         });
